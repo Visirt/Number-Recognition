853f988ee8939adfd05d069193da2568b31d18de
Paris <parisbielsa@gmail.com>
2021-08-07 21:11:24 +0200

    Finished frontend display

--- Number Recognition/Controllers/HomeController.cs
+++ Number Recognition/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Number_Recognition.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,15 +25,36 @@ namespace Number_Recognition.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(IFormFile file)
+        {
+            try
+            {
+                if (file != null)
+                {
+                    byte[] bytes = new byte[file.Length];
+                    using (var stream = file.OpenReadStream())
+                    {
+                        stream.Read(bytes, 0, (int)file.Length);
+                        stream.Close();
+                    }
+                    ViewBag.bytes = Convert.ToBase64String(bytes);
+
+                    ViewBag.Message = "File Uploaded Successfully!!";
+                    return View("Index");
+                }
+            }
+            catch { }
+
+            ViewBag.Message = "File upload failed!!";
+            ViewBag.filePath = null;
+            return View("Index");
         }
     }
 }
---
+++ Number Recognition/Models/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Number_Recognition.Models
+{
+    public class HomeViewModel
+    {
+        public IFormFile file { get; set; }
+    }
+}

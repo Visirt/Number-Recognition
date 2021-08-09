c3de413d64eaafa37d8d2d2fdcd44526ee990c4b
Paris <parisbielsa@gmail.com>
2021-08-09 19:46:05 +0200

    Gotten all pixels of the image

--- Number Recognition/Controllers/HomeController.cs
+++ Number Recognition/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using Number_Recognition.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -39,11 +40,14 @@ namespace Number_Recognition.Controllers
                 if (file != null)
                 {
                     byte[] bytes = new byte[file.Length];
+                    Bitmap png = null;
                     using (var stream = file.OpenReadStream())
                     {
                         stream.Read(bytes, 0, (int)file.Length);
+                        png = (Bitmap)Image.FromStream(stream);
                         stream.Close();
                     }
+                    var pixels = getPixels(png);
                     ViewBag.bytes = Convert.ToBase64String(bytes);
 
                     ViewBag.Message = "File Uploaded Successfully!!";
@@ -55,6 +59,22 @@ namespace Number_Recognition.Controllers
             ViewBag.Message = "File upload failed!!";
             ViewBag.filePath = null;
             return View("Index");
+        }
+
+        private byte[] getPixels(Bitmap png)
+        {
+            int width = png.Width;
+            int height = png.Height;
+            int numPixels = width * height;
+            byte[] pixels = new byte[numPixels];
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                {
+                    var color = png.GetPixel(x, y);
+                    var brightness = (byte)(color.GetBrightness()*256);
+                    pixels[y * width + x] = brightness;
+                }
+            return pixels;
         }
     }
 }
